9aa04ce9bb269c63d1387e1fec98c1e56fb13360
Werbson Yuri <yuri.luz25@icloud.com>
2023-05-05 12:31:18 -0300

    exercicio1

---
+++ Exercicio.rb
@@ -0,0 +1,11 @@
+print "Digite seu nome:         "
+nome1 = gets.chomp
+
+print "Digite seu sobrenome:     "
+nome2 = gets.chomp
+
+print "Digite sua idade:  "
+idade = gets.chomp
+
+
+puts "Olá #{nome1} #{nome2} sua idade é #{idade}."
